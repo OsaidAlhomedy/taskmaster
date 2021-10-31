647328133f211cd6c36feb65a81d380ee8c1d230
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-10-31 18:34:52 +0200

    added the counter functionalty

--- app/src/main/java/com/osaid/taskmaster/MainActivity2.java
+++ app/src/main/java/com/osaid/taskmaster/MainActivity2.java
@@ -3,20 +3,40 @@ package com.osaid.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    public int tasksNumbers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView tasks = findViewById(R.id.textView6);
+        tasksNumbers = Integer.parseInt(tasks.getText().toString());
+
     }
 
     public void taskAdd(View view) {
+        TextView tasks = findViewById(R.id.textView6);
+        tasksNumbers++;
+        tasks.setText(String.valueOf(tasksNumbers));
         Toast toast = Toast.makeText(getApplicationContext(),"Submitted !" , Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public int getTasksNumbers() {
+        return tasksNumbers;
+    }
+
+    public void setTasksNumbers(int tasksNumbers) {
+        this.tasksNumbers = tasksNumbers;
     }
 }
