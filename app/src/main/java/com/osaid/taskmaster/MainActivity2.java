edd690446b397d30a8bc0b146f1e744f8ccebf5b
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-10-31 18:06:58 +0200

    forgot to add the toast , re added it

--- app/src/main/java/com/osaid/taskmaster/MainActivity2.java
+++ app/src/main/java/com/osaid/taskmaster/MainActivity2.java
@@ -2,7 +2,10 @@ package com.osaid.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void taskAdd(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(),"Submitted !" , Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
