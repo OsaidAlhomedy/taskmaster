c2abcc382111b9a34636aed2fc47cca0bea3e545
OsaidAlhomedy <o.alhomedy@gmail.com>
2021-11-15 23:10:51 +0200

    added the s3 functionalty

--- app/src/main/java/com/osaid/taskmaster/LogIn.java
+++ app/src/main/java/com/osaid/taskmaster/LogIn.java
@@ -17,6 +17,7 @@ import com.amplifyframework.AmplifyException;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 
 import java.util.prefs.Preferences;
 
@@ -80,6 +81,7 @@ public class LogIn extends AppCompatActivity {
         try {
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
             Amplify.configure(getApplicationContext());
 
